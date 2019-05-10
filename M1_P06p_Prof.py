9c656aaf9556b1eaa6988d7828a9f33237166993
franjagon <fco.javier.gonzalvez@gmail.com>
2019-05-10 20:49:59 +0200

    Curso Python - Modulo 1 - Programa 6 - Hecho por R.M. Programa Principal

---
+++ M1_P06p_Prof.py
@@ -0,0 +1,6 @@
+import M1_P06m_Prof as MP
+import M1_P06m as MM
+
+print("El área de un triángulo de base 3.5 y altura 5 calculada por Ramón es:\t\t\t{}".format(MP.areaT(3.5, 5)))
+
+print("El área de un triángulo de base 2.5 y altura 15 calculada por Javier es:\t{}".format(MM.areaT(2.5, 15)))
