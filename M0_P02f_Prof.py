c99c109b822ca1c57c36121cdcabf18e08b05fc7
franjagon <fco.javier.gonzalvez@gmail.com>
2019-05-10 20:34:08 +0200

    Curso Python - Modulo 0 - Programa 2 - Hecho por R.M. con bucle for

---
+++ M0_P02f_Prof.py
@@ -0,0 +1,11 @@
+# Modulo 0 -  Programa 2 con for-in-range de Ramón Maldonado
+#
+#    Instrucciones
+#        - sumar los 100 primeros números con bucle for
+
+sumaTotal = 0
+
+for contador in range (1, 101):
+    sumaTotal = sumaTotal + contador
+    
+print("Total:", sumaTotal)
