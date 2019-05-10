4983254cef92139e72eaa74514a83effca7e3e11
franjagon <fco.javier.gonzalvez@gmail.com>
2019-05-10 20:34:26 +0200

    Curso Python - Modulo 0 - Programa 2 - Hecho por R.M. con bucle while

---
+++ M0_P02w_Prof.py
@@ -0,0 +1,13 @@
+# Modulo 0 -  Programa 2 con while de Ramón Maldonado
+#
+#    Instrucciones
+#        - sumar los 100 primeros números con bucle while
+
+sumaTotal = 0
+contador = 1
+
+while contador <= 100:
+    sumaTotal = sumaTotal + contador
+    contador = contador + 1
+    
+print("Total:", sumaTotal)
