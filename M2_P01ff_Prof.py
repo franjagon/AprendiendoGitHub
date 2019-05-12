11d34007c3dcf664c911e3182cba8a0ae79c9977
franjagon <fco.javier.gonzalvez@gmail.com>
2019-05-12 10:33:46 +0200

    Curso Python - Modulo 2 - Programa 1 - Hecho por R.M. Funciones como parametros de entrada de funciones.

--- M2_P01ff_Prof.py
+++ M2_P01ff_Prof.py
@@ -2,15 +2,21 @@ def normal(x):
     return x
 
 def cuadrado(x):
-    return x**2
+    return x ** 2
 
+def cubo(x):
+    return x ** 3
+
+'''Esta función realizará la suma de Todos los resultados de la iteración de la funcion que invoquemos'''
 def sumaTodos(limite, func):
     resultado = 0
     for i in range(0, limite + 1):
         resultado += func(i)
     return resultado
 
-'''Las funciones se pueden utilizar como parametros de entrada de otras funciones.'''
+'''Las funciones se pueden utilizar como parametros de entrada/salida de otras funciones.'''
 
-print("La suma de los 100 primeros números es: {}".format(sumaTodos(100, normal)))
-print("La suma de los cuadrados de los 4 primeros números es: {}".format(sumaTodos(4, cuadrado)))
+if __name__ == "__main__":
+    print("La suma de los 100 primeros números es: {}".format(sumaTodos(100, normal)))
+    print("La suma de los cuadrados de los 4 primeros números es: {}".format(sumaTodos(4, cuadrado)))
+    print("La suma de los cubos de los 3 primeros números es: {}".format(sumaTodos(3, cubo)))
