9454c245352bfc2c3e400b00614dd7f5e271a3e6
franjagon <fco.javier.gonzalvez@gmail.com>
2019-05-12 12:19:10 +0200

    Curso Python - Modulo 2 - Programa 2 - Hecho por R.M. Map - Filter - Reduce

--- M2_P02mfr_Prof.py
+++ M2_P02mfr_Prof.py
@@ -3,9 +3,19 @@ from functools import reduce
 lista = [1, 3, -2, 15, 9]
 
 print("Tomamos esta lista", list(lista))
-print("La lista cuyos elementos son el doble (dobles):", list(map(lambda x : x * 2, list(lista))))
+print("\nAhora haremos print con los cálculos lambda embebidos\n")
+print("La lista cuyos elementos son el doble sería:", list(map(lambda x : x * 2, list(lista))))
 print("Los elementos pares de la lista:", list(filter(lambda x : x % 2 == 0, list(lista))))
 print("Los elementos pares de la lista de dobles:", list(filter(lambda x : x % 2 == 0, list(map(lambda x : x * 2, list(lista))))))
+
+listaDobles = map(lambda x : x * 2, lista)
+listaPares1 = filter(lambda x : x % 2 == 0, lista)
+listaPares2 = filter(lambda x : x % 2 == 0, list(listaDobles))
+
+print("\nY a continuación haremos print con los cálculos lambda hechos con antelación\n")
+print("La lista cuyos elementos son el doble sería:", list(listaDobles))
+print("Los elementos pares de la lista:", list(listaPares1))
+print("Los elementos pares de la lista de dobles:", list(listaPares2))
 
 sumatorio = reduce(lambda x, y : x + y, list(lista))
 sumadoble = reduce(lambda x, y : x + (y * 2), list(lista))
