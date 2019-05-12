dc3f8b8b019a8dbf5ff40cf04b10166d4e003053
franjagon <fco.javier.gonzalvez@gmail.com>
2019-05-12 13:49:52 +0200

    Curso Python - Modulo 2 - Programa 4 - Hecho por R.M. - Objeto Turtle

---
+++ M2_P04_Prof.py
@@ -0,0 +1,19 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otraTortuguita = turtle.Turtle()
+yOtraTortuguita = turtle.Turtle()
+
+tortuguita.shape("arrow")
+tortuguita.color("blue")
+tortuguita.fd(50)
+
+otraTortuguita.shape("turtle")
+otraTortuguita.color("red")
+otraTortuguita.left(70)
+otraTortuguita.fd(100)
+
+yOtraTortuguita.shape("circle")
+yOtraTortuguita.color("green")
+yOtraTortuguita.left(135)
+yOtraTortuguita.fd(150)
